320e241bc1850deeaabe718c183f9d754ca11aaf
Antônio Carlos <juniorbecari@gmail.com>
2022-07-06 14:54:57 -0300

    Modified Shotgun's bullet sprite, so looks like it shoots 3 bullets at a time

--- src/scripts/ts/Weapon.ts
+++ src/scripts/ts/Weapon.ts
@@ -8,7 +8,7 @@ function getWeapon(weapon: WeaponType): Weapon | null {
         return new Weapon({x: 0, y: 0, w: 44 * 3, h: 16 * 3}, weapons, {x: 32 * 3, y: 0, w: 44 * 3, h: 16 * 3}, {x: 0, y: 0, w: 4 * 3, h: 4 * 3}, {x: 0, y: 16 * 3, w: 4 * 3, h: 4 * 3}, 5, 30, 1, 10, 400, 20, "Rifle");
     }
     else if (weapon === "shotgun") {
-        return new Weapon({x: 0, y: 0, w: 44 * 3, h: 16 * 3}, weapons, {x: 80 * 3, y: 0, w: 44 * 3, h: 16 * 3}, {x: 0, y: 0, w: 4 * 3, h: 4 * 3}, {x: 0, y: 16 * 3, w: 4 * 3, h: 4 * 3}, 7, 30, 1, 40, 350, 5, "Shotgun");
+        return new Weapon({x: 0, y: 0, w: 44 * 3, h: 16 * 3}, weapons, {x: 80 * 3, y: 0, w: 44 * 3, h: 16 * 3}, {x: 0, y: 0, w: 4 * 3, h: 40}, {x: 16, y: 16 * 3, w: 4 * 3, h: 40}, 7, 30, 1, 40, 350, 5, "Shotgun");
     }
     
     return null;
